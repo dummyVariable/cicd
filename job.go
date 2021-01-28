f6b38e258d4b1aecde9fda4fd77d1cd097ba264b
dummyvariable <officialsanjivee@gmail.com>
2021-01-28 18:07:56 +0530

    added job function for parsing jobs from file object

--- job.go
+++ job.go
@@ -1,8 +1,26 @@
 package cicd
 
+import (
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
 //Job defines a structure of job instuctions
 type Job struct {
 	PreBuild  []string `yaml:"prebuild"`
 	Build     []string `yaml:"build"`
 	PostBuild []string `yaml:"postbuild"`
+}
+
+func jobs(file []byte) Job {
+
+	var jobs Job
+
+	err := yaml.Unmarshal(file, &jobs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return jobs
 }
