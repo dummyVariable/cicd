765cff06a6b0787fc59ea943286fef3d3823702e
dummyvariable <officialsanjivee@gmail.com>
2021-01-28 19:01:49 +0530

    added tests for processor functionality

---
+++ processor_test.go
@@ -0,0 +1,35 @@
+package cicd
+
+import (
+	"testing"
+)
+
+func Test_build(t *testing.T) {
+
+	tests := []struct {
+		name       string
+		statements []string
+		wantErr    bool
+	}{
+		{
+			name:       "build single commands",
+			statements: []string{"echo lol"},
+			wantErr:    false,
+		}, {
+			name:       "build multiple commands",
+			statements: []string{"echo lol", "echo lol"},
+			wantErr:    false,
+		}, {
+			name:       "build no commands",
+			statements: []string{},
+			wantErr:    false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := build(tt.statements); (err != nil) != tt.wantErr {
+				t.Errorf("build() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
